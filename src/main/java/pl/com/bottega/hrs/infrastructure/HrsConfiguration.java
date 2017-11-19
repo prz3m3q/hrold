3625846af4a2c6c8135b26a57bc6c02e0c962bc4
Maciej Zubala <maciej.zubala@gmail.com>
2017-11-19 10:20:11 +0100

    formatting dates

--- src/main/java/pl/com/bottega/hrs/infrastructure/HrsConfiguration.java
+++ src/main/java/pl/com/bottega/hrs/infrastructure/HrsConfiguration.java
@@ -3,9 +3,11 @@ package pl.com.bottega.hrs.infrastructure;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import pl.com.bottega.hrs.model.repositories.DepartmentRepository;
 
 import javax.persistence.EntityManager;
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class HrsConfiguration {
@@ -15,12 +17,20 @@ public class HrsConfiguration {
 
     @Bean
     public DepartmentRepository departmentRepository(EntityManager entityManager) {
-        if(departmentRepositoryStrategy.equals("csv"))
+        if (departmentRepositoryStrategy.equals("csv"))
             return new CSVDepartmentRepository();
         else if (departmentRepositoryStrategy.equals("jpa"))
             return new JPADepartmentRepository(entityManager);
         else
             throw new IllegalArgumentException("I don't know ;(");
+    }
+
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm"));
+        return builder;
     }
 
 }
